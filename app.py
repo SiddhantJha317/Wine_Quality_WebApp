e12d3b001ca7b66cc25e850b8983c82e5a8496f2
Siddhant <siddhantjha365@gmail.com>
2022-08-25 16:01:44 +0530

    hyperparameteres tuned and data scaled

--- app.py
+++ app.py
@@ -3,21 +3,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn import preprocessing
+from sklearn.model_selection import GridSearchCV
+
 #app heading
 st.write("""
-# Wine Quality Prediction App
-This app predicts the ***Wine Quality*** type!
+# My Wine Quality Prediction App
+This app predicts the ***Wine Quality*** that's it btw
 """)
 #creating sidebar for user input features
 st.sidebar.header('User Input Parameters')
   
 def user_input_features():
-        fixed_acidity = st.sidebar.slider('fixed acidity', 4.6, 15.9, 8.31)
-        volatile_acidity = st.sidebar.slider('volatile acidity', 0.12,1.58 , 0.52)
+        fixed_acidity = st.sidebar.slider('fixed acidity', 4.0, 16.0, 12.6)
+        volatile_acidity = st.sidebar.slider('volatile acidity', 0.10,1.6 , 0.34)
         citric_acid = st.sidebar.slider('citric acid', 0.0,1.0 , 0.5)
-        chlorides = st.sidebar.slider('chlorides', 0.01,0.6 , 0.08)
-        total_sulfur_dioxide=st.sidebar.slider('total sulfur dioxide', 6.0,289.0 , 46.0)
-        alcohol=st.sidebar.slider('alcohol', 8.4,14.9, 10.4)
+        chlorides = st.sidebar.slider('chlorides', 0.01,0.34 , 0.04)
+        total_sulfur_dioxide=st.sidebar.slider('total sulfur dioxide', 6.0,280.0 , 31.4)
+        alcohol=st.sidebar.slider('alcohol', 8.42,15.0, 8.9)
         sulphates=st.sidebar.slider('sulphates', 0.33,2.0,0.65 )
         data = {'fixed_acidity': fixed_acidity,
                 'volatile_acidity': volatile_acidity,
@@ -30,21 +34,25 @@ def user_input_features():
         return features
 df = user_input_features()
 
-st.subheader('User Input parameters')
+st.subheader('User Input')
 st.write(df)
 #reading csv file
 data=pd.read_csv("winequality-red.csv")
 X =np.array(data[['fixed acidity', 'volatile acidity' , 'citric acid' , 'chlorides' , 'total sulfur dioxide' , 'alcohol' , 'sulphates']])
 Y = np.array(data['quality'])
 #random forest model
-rfc= RandomForestClassifier()
-rfc.fit(X, Y)
-st.subheader('Wine quality labels and their corresponding index number')
+pipeline=make_pipeline(preprocessing.StandardScaler(),
+                        RandomForestClassifier(n_estimators=100))
+hyperparameters = { 'randomforestclassifier__max_features' : ['auto', 'sqrt', 'log2'],
+                   'randomforestclassifier__max_depth' : [None, 10, 7, 5, 3, 1]}                        
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+clf.fit(X, Y)
+st.subheader('Wine quality labels')
 st.write(pd.DataFrame({
    'wine quality': [3, 4, 5, 6, 7, 8 ]}))
 
-prediction = rfc.predict(df)
-prediction_proba = rfc.predict_proba(df)
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
 st.subheader('Prediction')
 st.write(prediction)
 
